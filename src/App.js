58aa64c99e481e6aa230c13ece5ade235f5b483b
Nirmal Basnet <nirmalbasnet4499@gmail.com>
2024-02-21 22:54:22 +0545

    refactor: used vars in workflow yaml and added abutton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const hooray = () => {
+    alert("Horray! the app is deployed");
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={hooray}>Learn React</button>
       </header>
     </div>
   );
